888fcdd3c7ab108475193023c5fe30c35f71cc45
Ranjeth-Sundaram1 <90901523+Ranjeth-Sundaram1@users.noreply.github.com>
2022-01-08 22:24:35 +0530

    Update print_color_pair_number_mapping.py

--- print_color_pair_number_mapping.py
+++ print_color_pair_number_mapping.py
@@ -1,7 +1,7 @@
 import get_color_pair_number_mapping 
 from get_color_pair_number_mapping import get_color_from_pair_number
 
-def print_color_number_mapping():
+def print_color_pair_number_mapping():
     print ('Pair number | Major color | Minor color \n')
     for number in range (len(get_color_pair_number_mapping.MINOR_COLORS)*len(get_color_pair_number_mapping.MAJOR_COLORS)):
         Major_color, Minor_color = get_color_from_pair_number(number+1)
