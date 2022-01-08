102f2498bb1e03b316c8f071599ff179003a3a12
Ranjeth-Sundaram1 <90901523+Ranjeth-Sundaram1@users.noreply.github.com>
2022-01-08 21:05:57 +0530

    Update get_color_pairnumber_combo.py

--- get_color_pairnumber_combo.py
+++ get_color_pairnumber_combo.py
@@ -2,24 +2,33 @@
 MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
 MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
 
-class get_color_pairnumber_combo():
-  def get_color_from_pair_number(pair_number):
+
+def color_pair_to_string(major_color, minor_color) -> str :
+    return f'{major_color} {minor_color}'
+
+def get_color_from_pair_number(pair_number) -> int:
     zero_based_pair_number = pair_number - 1
     major_index = zero_based_pair_number // len(MINOR_COLORS)
     if major_index >= len(MAJOR_COLORS):
-      raise Exception('Major index out of range')
+        raise Exception('Major index out of range')
     minor_index = zero_based_pair_number % len(MINOR_COLORS)
     if minor_index >= len(MINOR_COLORS):
-      raise Exception('Minor index out of range')
+        raise Exception('Minor index out of range')
     return MAJOR_COLORS[major_index], MINOR_COLORS[minor_index]
 
-  def get_pair_number_from_color(major_color, minor_color):
+def get_pair_number_from_color(major_color, minor_color):
     try:
-      major_index = MAJOR_COLORS.index(major_color)
+        major_index = MAJOR_COLORS.index(major_color)
     except ValueError:
-      raise Exception('Major index out of range')
+        raise Exception('Major index out of range')
     try:
-      minor_index = MINOR_COLORS.index(minor_color)
+        minor_index = MINOR_COLORS.index(minor_color)
     except ValueError:
-      raise Exception('Minor index out of range')
+        raise Exception('Minor index out of range')
     return major_index * len(MINOR_COLORS) + minor_index + 1
+
+def print_color_number_combo():
+    print ('Pair number | Major color | Minor color \n')
+    for number in range (1, (len(MINOR_COLORS)*len(MAJOR_COLORS))+1):
+        Major_color, Minor_color = get_color_from_pair_number(number)
+        print (f'{number} | {Major_color} | {Minor_color} \n')
