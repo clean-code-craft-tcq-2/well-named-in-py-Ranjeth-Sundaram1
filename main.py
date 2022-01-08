a9458e39cda41695ad78857481cb99a2797853c1
Ranjeth-Sundaram1 <90901523+Ranjeth-Sundaram1@users.noreply.github.com>
2022-01-08 22:15:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from test_color_pair_number_mapping import test_number_to_pair
 from test_color_pair_number_mapping import test_pair_to_number
-from get_color_pair_number_mapping import print_color_pair_number_mapping
+from print_color_pair_number_mapping import print_color_pair_number_mapping
 
 if __name__ == '__main__':
     
